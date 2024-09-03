836ad587c77916051bd06bc9a14849ee7bb4edaf
dcaizaguano <dcaizaguano@oscus.coop>
2024-09-03 10:25:22 -0500

    configuracion de peticiones http  BASICA

---
+++ lib/data/servicios/servicios.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_application_2/data/servicios/autenticacion/autenticacion_aws_services.dart';
--- lib/presentacion/screens/validacionUsuarios/proceso_validacion_screen.dart
+++ lib/presentacion/screens/validacionUsuarios/proceso_validacion_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/data/servicios/servicios.dart';
 import 'package:flutter_application_2/presentacion/providers/cedula_provider.dart';
 import 'package:flutter_application_2/presentacion/providers/codigo_dactilar_provider.dart';
 import 'package:flutter_application_2/presentacion/providers/foto_provider.dart';
@@ -27,18 +28,21 @@ class _validaciones extends ConsumerWidget {
       child: Column(
         children: [
           IconButton(
-              onPressed: () {
-                final cedula = ref.read(cedulaProvider);
-                final codigoDactilar = ref.read(codigoDactilarProvider);
-                final foto = ref.read(fotoProvider);
-
-                print('LA CEDULA ES:  $cedula');
-                print('EL CODIGO DACTILAR ES:  $codigoDactilar');
-                print('LA FOTO ES :  $foto');
-              },
+              onPressed: () => _crearSessionID(context),
               icon: Icon(Icons.access_time_sharp))
         ],
       ),
+    );
+  }
+}
+
+Future<void> _crearSessionID(BuildContext context) async {
+  final autenticacionAwsServices = AutenticacionAwsServices();
+  try {
+    await autenticacionAwsServices.getSessionID();
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Error al comparar rostros: $e')),
     );
   }
 }
